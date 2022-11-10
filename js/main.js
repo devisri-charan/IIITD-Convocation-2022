3bc90b3ad01e5547df025d91f5e6236b91ea51b7
DeviSriCharan20412 <valupadasu20412@iiitd.ac.in>
2022-11-10 14:29:57 +0530

    Added Carousel Back

--- js/main.js
+++ js/main.js
@@ -3,8 +3,6 @@
 
   $(document).ready(function() {
     if($(window).width() < 515) {
-        $("#carousel1").attr("src", "img/Carousel/mobile1.png");
-        $("#carousel2").attr("src", "img/Carousel/mobile2.png");
         $("#hero").attr("src", "img/Carousel/hero.png");
         $("#hero1").attr("src", "img/Carousel/hero.png");
     }
